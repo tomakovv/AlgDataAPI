3bd5b7e50805740baaafbea104a60326262468c8
Tomek Gawron <tomekgawron621@gmail.com>
2022-08-18 14:40:26 +0200

    Add base projects

---
+++ src/AlgDataGatewayAPI/AlgDataGateway/Controllers/ProxyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlgDataGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProxyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        private async Task<ContentResult> ProxyTo(string url)
+      => Content(await _httpClient.GetStringAsync(url));
+    }
+}
